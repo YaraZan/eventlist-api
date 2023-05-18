182e1e34d46c6432a13a20053b4beafc1590f983
YaraZan <a9172607998@gmail.com>
2023-05-18 11:28:02 +0300

    "initial"

--- api/event/read.php
+++ api/event/read.php
@@ -9,3 +9,26 @@
     // Instantiate DB & connect
     $database = new Database();
     $db = $database->connect();
+
+    // Instantinate event object
+    $event = new Event($db);
+
+    // Event query
+    $result = $post->read();
+
+    // Get row count
+    $num = $result->rowCount();
+
+    // Check if any events
+    if ($num > 0) {
+        $posts_arr = array();
+        $posts_arr['data'] = array();
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $row['title']
+        }
+
+    } else {
+
+    }
+?>
