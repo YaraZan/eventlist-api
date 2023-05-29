5ed375c815666c5ab85b14aa6c681a78a882ec40
YaraZan <a9172607998@gmail.com>
2023-05-29 14:36:49 +0300

    add: Organisation class

---
+++ models/Organisation.php
@@ -0,0 +1,153 @@
+<?php
+    class Organisation {
+        // DB stuff
+        private $conn;
+        private $table = 'organisations';
+
+        // Properties
+        public $id;
+        public $name;
+        public $email;
+        public $descr;
+        public $target;
+        public $level;
+        public $type;
+        public $location;
+        public $max_people;
+
+        // Constructor with DB
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        // Get Organisations
+        public function read() {
+            // Create query
+            $query = 'SELECT
+                        ta.name as target_name,
+                        l.name as level_name,
+                        ty.name as type_name,
+                        o.id,
+                        o.name,
+                        o.email,
+                        o.descr,
+                        o.target,
+                        o.level,
+                        o.type,
+                        o.location,
+                        o.max_people
+                    FROM 
+                        ' . $this->table . ' o
+                    LEFT JOIN org_levels l ON o.level = l.id
+                    LEFT JOIN org_targets ta ON o.target = ta.id
+                    LEFT JOIN org_types ty ON o.type = ty.id
+                    ORDER BY
+                        o.created_at DESC';
+
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Execute query 
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        // Get single event
+        public function read_single() {
+            $query = 'SELECT
+                        ta.name as target_name,
+                        l.name as level_name,
+                        ty.name as type_name,
+                        o.id,
+                        o.name,
+                        o.email,
+                        o.descr,
+                        o.target,
+                        o.level,
+                        o.type,
+                        o.location,
+                        o.max_people
+                    FROM 
+                        ' . $this->table . ' o
+                    LEFT JOIN org_levels l ON o.level = l.id
+                    LEFT JOIN org_targets ta ON o.target = ta.id
+                    LEFT JOIN org_types ty ON o.type = ty.id
+                    ORDER BY
+                        o.created_at DESC
+                    WHERE 
+                        o.id = ?
+                        LIMIT 0,1';
+            
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Bind ID
+            $stmt->bindParam(1, $this->id);
+            
+            // Execute query 
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->id = $row['id'];
+            $this->name = $row['name'];
+            $this->email = $row['email'];
+            $this->descr = $row['descr'];
+            $this->target = $row['target'];
+            $this->level = $row['level'];
+            $this->type = $row['type'];
+            $this->location = $row['location'];
+            $this->max_people = $row['max_people'];
+        }
+
+        // Create Organisation
+        public function create() {
+            // Create query
+            $query = 'INSERT INTO ' . $this->table . '
+                SET
+                    name = :name,
+                    email = :email,
+                    descr = :descr,
+                    target = :target,
+                    level = :level,
+                    type = :type,
+                    location = :location,
+                    max_people = :max_people
+            ';
+
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+
+            // Clean data
+            $this->name = htmlspecialchars(strip_tags($this->name));
+            $this->email = htmlspecialchars(strip_tags($this->email));
+            $this->descr = htmlspecialchars(strip_tags($this->descr));
+            $this->target = htmlspecialchars(strip_tags($this->target));
+            $this->level = htmlspecialchars(strip_tags($this->level));
+            $this->type = htmlspecialchars(strip_tags($this->type));
+            $this->location = htmlspecialchars(strip_tags($this->location));
+            $this->max_people = htmlspecialchars(strip_tags($this->max_people));
+
+            // Bind data
+            $stmt->bindParam(':name', $this->name);
+            $stmt->bindParam(':email', $this->email);
+            $stmt->bindParam(':descr', $this->descr);
+            $stmt->bindParam(':target', $this->target);
+            $stmt->bindParam(':level', $this->level);
+            $stmt->bindParam(':type', $this->type);
+            $stmt->bindParam(':location', $this->location);
+            $stmt->bindParam(':max_people', $this->max_people);
+
+            // Execute query
+            if($stmt->execute()) {
+                return true;
+            }
+
+            // Print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+
+            return false;
+        }
+    }
+?>
