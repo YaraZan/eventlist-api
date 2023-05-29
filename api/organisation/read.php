c01bb391c85133897066cec4ca70132ae7c9f762
YaraZan <a9172607998@gmail.com>
2023-05-29 14:36:30 +0300

    add: Reading orgs

---
+++ api/organisation/read.php
@@ -0,0 +1,54 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Organisation.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantinate event object
+    $org = new Organisation($db);
+
+    // Event query
+    $result = $org->read();
+
+    // Get row count
+    $num = $result->rowCount();
+
+    // Check if any events
+    if ($num > 0) {
+        $orgs_arr = array();
+        $orgs_arr['data'] = array();
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $org_item = array(
+                'id' => $id,
+                'name' => $name,
+                'email' => $email,
+                'descr' => $descr,
+                'target' => $target,
+                'level' => $level,
+                'type' => $type,
+                'location' => $location,
+                'max_people' => $max_people
+            );
+
+            // Push to "Data"
+            array_push($orgs_arr['data'], $org_item);
+        }
+
+        // Turn to JSON
+        echo json_encode($orgs_arr);
+
+    } else {
+        echo json_encode(
+            array('message' => 'No organisations found')
+        );
+    }
+?>
